57085b4aa6d3da325eeb1220bf3871e7a6d8b4cb
kwakseobang <iii6602@naver.com>
2025-12-01 12:56:37 +0900

    Test[#1] : 테스트 Support Class 생성

---
+++ src/test/java/org/squad/careerhub/ControllerTestSupport.java
@@ -0,0 +1,19 @@
+package org.squad.careerhub;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+@WebMvcTest(
+        controllers = {
+        })
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvcTester mvcTester;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}
