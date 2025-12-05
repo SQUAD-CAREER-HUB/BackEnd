1ee301de2790fd3c2eaa2fdd97b2c309c8f27407
kwakseobang <iii6602@naver.com>
2025-12-05 22:16:44 +0900

    Feat[#6]: 커스텀 LoginMember 인증 어노테이션 처리를 위한 LoginMemberArgumentResolver 및 WebConfig 구현

--- src/main/java/org/squad/careerhub/global/annotation/LoginMember.java
+++ src/main/java/org/squad/careerhub/global/annotation/LoginMember.java
@@ -6,11 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/*
- * 현재 Mock 스웨거 API 작성을 위해 작동하지 않는 Custom Annotation을 생성했습니다.
- * 이 Custom Annotation은 현재 작동되지 않으며 추후 Spring Security 관련 기능 구현 후 정상적으로 작동될 예정입니다.
- */
-
 @Parameter(hidden = true)
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
