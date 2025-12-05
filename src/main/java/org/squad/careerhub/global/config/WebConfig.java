66b5f4c1249b7c885afb0d86be80896a30132d78
kwakseobang <iii6602@naver.com>
2025-12-05 22:16:44 +0900

    Feat[#6]: 커스텀 LoginMember 인증 어노테이션 처리를 위한 LoginMemberArgumentResolver 및 WebConfig 구현

---
+++ src/main/java/org/squad/careerhub/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.squad.careerhub.global.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.squad.careerhub.global.annotation.resolver.LoginMemberArgumentResolver;
+
+@RequiredArgsConstructor
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
+    }
+
+}
