83b0223e08029563c649cfa7df6f58eeafa11fab
Hm-source <swo8852@gmail.com>
2025-12-12 03:42:37 +0900

    Feat[#21]: JobPostingService 매니저 로직 및 검증 로직 분리

--- src/main/java/org/squad/careerhub/domain/jobposting/service/JobPostingService.java
+++ src/main/java/org/squad/careerhub/domain/jobposting/service/JobPostingService.java
@@ -1,10 +1,25 @@
 package org.squad.careerhub.domain.jobposting.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.squad.careerhub.domain.jobposting.service.dto.JobPostingContentReadResult;
+import org.squad.careerhub.domain.jobposting.service.dto.response.JobPostingExtractResponse;
+import org.squad.careerhub.domain.jobposting.service.port.JobPostingExtractionPort;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class JobPostingService {
+    private final JobPostingExtractionPort jobPostingExtractionPort;
+    private final JobPostingValidator jobPostingValidator;
+    private final JobPostingManager jobPostingManager;
 
+    public JobPostingExtractResponse extractJobPosting(String url) {
+        jobPostingValidator.validateJobPostingUrl(url);
+        JobPostingContentReadResult result = jobPostingManager.getJobPostingContent(url);
+        jobPostingValidator.validateReadResult(result);
+
+        return jobPostingManager.extractJobPosting(result);
+    }
 }
