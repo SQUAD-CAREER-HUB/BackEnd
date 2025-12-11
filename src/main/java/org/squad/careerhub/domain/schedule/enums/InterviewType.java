7b9ca92adfd073412177d7883119df459e7d2bb1
kwakseobang <iii6602@naver.com>
2025-12-11 11:12:07 +0900

    Refactor[#27]: Align enum value formatting for consistency

--- src/main/java/org/squad/careerhub/domain/schedule/enums/InterviewType.java
+++ src/main/java/org/squad/careerhub/domain/schedule/enums/InterviewType.java
@@ -7,12 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum InterviewType {
 
-    TECH("기술 면접"),
-    FIT("컬처핏/인성 면접"),
-    EXECUTIVE("임원 면접"),
-    DESIGN("시스템 디자인 면접"),
-    TEST("라이브 코딩 테스트 면접"),
-    OTHER("기타");
+    TECH      ("기술 면접"),
+    FIT       ("컬처핏/인성 면접"),
+    EXECUTIVE ("임원 면접"),
+    DESIGN    ("시스템 디자인 면접"),
+    TEST      ("라이브 코딩 테스트 면접"),
+    OTHER     ("기타");
 
     private final String description;
 
