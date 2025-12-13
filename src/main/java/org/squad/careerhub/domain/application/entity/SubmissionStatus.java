13d500861125a975ccb39f0d378ea166834bbe5d
kwakseobang <iii6602@naver.com>
2025-12-13 15:23:10 +0900

    Feat[#27]: Add enums for application stage and submission status

---
+++ src/main/java/org/squad/careerhub/domain/application/entity/SubmissionStatus.java
@@ -0,0 +1,12 @@
+package org.squad.careerhub.domain.application.entity;
+
+/**
+ * 서류 제출 상태를 나타내는 열거형입니다.
+ **/
+
+public enum SubmissionStatus {
+
+    NOT_SUBMITTED, // 미제출
+    SUBMITTED      // 제출 완료
+
+}
