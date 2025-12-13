14c4b7a801fecb96952cee90f8ff6cac2f5cac0b
Hm-source <swo8852@gmail.com>
2025-12-13 22:49:58 +0900

    Fix[#21]: Port interface @Component 제거

--- src/main/java/org/squad/careerhub/domain/jobposting/service/port/JobPostingContentReaderPort.java
+++ src/main/java/org/squad/careerhub/domain/jobposting/service/port/JobPostingContentReaderPort.java
@@ -1,9 +1,7 @@
 package org.squad.careerhub.domain.jobposting.service.port;
 
-import org.springframework.stereotype.Component;
 import org.squad.careerhub.domain.jobposting.service.dto.JobPostingContentReadResult;
 
-@Component
 public interface JobPostingContentReaderPort {
     JobPostingContentReadResult read(String url);
 }
