23b23314c7eb3d223cde65b19c82fd62c57d0285
Hm-source <swo8852@gmail.com>
2025-12-19 02:33:48 +0900

    Fix[#30]: 면접, 기타 전형 dto 및 서비스 변경에 따른 삭제

--- src/main/java/org/squad/careerhub/domain/schedule/repository/InterviewScheduleJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.squad.careerhub.domain.schedule.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.squad.careerhub.domain.schedule.entity.InterviewSchedule;
-
-public interface InterviewScheduleJpaRepository extends JpaRepository<InterviewSchedule, Long> {
-
-}
