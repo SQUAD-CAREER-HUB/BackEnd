f8ac1f64d4d7d62d458328afea502fe60a22f4b1
Hm-source <swo8852@gmail.com>
2025-12-19 02:23:51 +0900

    Feat[#30]: 컨트롤러 수정

---
+++ src/main/java/org/squad/careerhub/domain/schedule/controller/ScheduleController.java
@@ -0,0 +1,74 @@
+package org.squad.careerhub.domain.schedule.controller;
+
+
+import jakarta.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.squad.careerhub.domain.schedule.controller.dto.EtcScheduleCreateRequest;
+import org.squad.careerhub.domain.application.entity.StageType;
+import org.squad.careerhub.domain.schedule.controller.dto.InterviewScheduleCreateRequest;
+import org.squad.careerhub.domain.schedule.service.ScheduleService;
+import org.squad.careerhub.domain.schedule.service.dto.ScheduleListResponse;
+import org.squad.careerhub.domain.schedule.service.dto.ScheduleResponse;
+import org.squad.careerhub.global.annotation.LoginMember;
+
+@RestController
+@RequestMapping("/v1/schedule")
+@RequiredArgsConstructor
+public class ScheduleController extends ScheduleDocsController {
+
+    private final ScheduleService scheduleService;
+
+    @Override
+    @PostMapping("/interview")
+    public ResponseEntity<ScheduleResponse> createInterviewFromCalendar(
+        @Valid @RequestBody InterviewScheduleCreateRequest request,
+        @LoginMember Long memberId
+    ) {
+        ScheduleResponse response = scheduleService.createInterviewFromCalendar(
+            request.toApplicationInfo(),
+            request.toNewInterviewSchedule(),
+            memberId
+        );
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @Override
+    @PostMapping("/etc")
+    public ResponseEntity<ScheduleResponse> createEtcFromCalendar(
+        @Valid @RequestBody EtcScheduleCreateRequest request,
+        @LoginMember Long memberId
+    ) {
+        ScheduleResponse response = scheduleService.createEtcFromCalendar(
+            request.toApplicationInfo(),
+            request.toNewEtcSchedule(),
+            memberId);
+        return ResponseEntity.status(201).body(response);
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<ScheduleListResponse> getSchedule(
+        @RequestParam
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+        @RequestParam
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+        @RequestParam
+        String companyName,
+        @RequestParam
+        List<StageType> stageTypes,
+        @LoginMember Long memberId
+    ) {
+        return ResponseEntity.ok(ScheduleListResponse.mock());
+    }
+
+}
