dfa482a3d49f9d09b224dfcc37cd5557c712306d
kwakseobang <iii6602@naver.com>
2025-12-20 09:36:44 +0900

    Refactor[#50]: StageType에 isDocument, isEtc, isInterview 메서드 추가 및 검증 로직 개선

--- src/main/java/org/squad/careerhub/domain/application/service/ApplicationPolicyValidator.java
+++ src/main/java/org/squad/careerhub/domain/application/service/ApplicationPolicyValidator.java
@@ -14,13 +14,13 @@ public class ApplicationPolicyValidator {
     // 지원서 생성 시 전형에 따른 입력 규칙 검증
     public void validateNewStage(NewStage newStage) {
         // 기타 전형 일정은 기타 전형일 때만 입력 가능
-        if (newStage.newEtcSchedule() != null && !newStage.stageType().equals(StageType.ETC)) {
+        if (newStage.newEtcSchedule() != null && !newStage.stageType().isEtc()) {
             throw new CareerHubException(ErrorStatus.INVALID_ETC_STAGE_RULE);
         }
 
         // 면접 일정은 면접 전형일 때만 입력 가능
         // 컨트롤러에서 면접 일정 DTO 가 NULL 일 경우 빈 리스트로 변환해주기 때문에 NULL 체크는 불필요
-        if (!newStage.newInterviewSchedules().isEmpty() && !newStage.stageType().equals(StageType.INTERVIEW)) {
+        if (!newStage.newInterviewSchedules().isEmpty() && !newStage.stageType().isInterview()) {
             throw new CareerHubException(ErrorStatus.INVALID_SCHEDULE_TYPE_RULE);
         }
     }
