67229f3a0e6488b6ba2bc332325843d5aee41e83
kwakseobang <iii6602@naver.com>
2025-12-20 10:44:34 +0900

    Feat[#50]: 지원서 생성 시 지원 종료 단계 검증 추가 및테스트 보강 및 Builder 패턴 적용

--- src/main/java/org/squad/careerhub/domain/application/service/ApplicationPolicyValidator.java
+++ src/main/java/org/squad/careerhub/domain/application/service/ApplicationPolicyValidator.java
@@ -13,15 +13,20 @@ public class ApplicationPolicyValidator {
 
     // 지원서 생성 시 전형에 따른 입력 규칙 검증
     public void validateNewStage(NewStage newStage) {
-        // 기타 전형 일정은 기타 전형일 때만 입력 가능
+        // 기타 전형 일정은 기타 전형 단계 일 때만 입력 가능
         if (newStage.newEtcSchedule() != null && !newStage.stageType().isEtc()) {
             throw new CareerHubException(ErrorStatus.INVALID_ETC_STAGE_RULE);
         }
 
-        // 면접 일정은 면접 전형일 때만 입력 가능
+        // 면접 일정은 면접 전형 단계일 때만 입력 가능
         // 컨트롤러에서 면접 일정 DTO 가 NULL 일 경우 빈 리스트로 변환해주기 때문에 NULL 체크는 불필요
         if (!newStage.newInterviewSchedules().isEmpty() && !newStage.stageType().isInterview()) {
             throw new CareerHubException(ErrorStatus.INVALID_SCHEDULE_TYPE_RULE);
+        }
+
+        // 지원서 최종 상태는 지원 종료 단계에서만 입력 가능
+        if (newStage.finalapplicationStatus() != null && !newStage.stageType().isApplicationClose()) {
+            throw new CareerHubException(ErrorStatus.INVALID_FINAL_APPLICATION_STATUS_RULE);
         }
     }
 
--- src/test/java/org/squad/careerhub/domain/application/service/ApplicationPolicyValidatorTest.java
+++ src/test/java/org/squad/careerhub/domain/application/service/ApplicationPolicyValidatorTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.squad.careerhub.TestDoubleSupport;
+import org.squad.careerhub.domain.application.entity.ApplicationStatus;
 import org.squad.careerhub.domain.application.entity.StageType;
 import org.squad.careerhub.domain.application.entity.SubmissionStatus;
 import org.squad.careerhub.domain.application.service.dto.NewEtcSchedule;
@@ -24,31 +25,40 @@ class ApplicationPolicyValidatorTest extends TestDoubleSupport {
     @Test
     void 기타_일정은_기타_전형일_때만_가능하다() {
         // given
-        var etcNewStage = new NewStage(
-                StageType.ETC,
-                null,
-                new NewEtcSchedule("코딩 테스트", LocalDateTime.now(), LocalDateTime.now().plusDays(2)),
-                List.of()
-        );
+        var etcNewStage = NewStage.builder()
+                .stageType(StageType.ETC)
+                .newEtcSchedule(new NewEtcSchedule("코딩 테스트", LocalDateTime.now(), LocalDateTime.now().plusDays(2)))
+                .newInterviewSchedules(List.of())
+                .build();
 
-        var documentStage = new NewStage(
-                StageType.DOCUMENT,
-                SubmissionStatus.SUBMITTED,
-                new NewEtcSchedule("코딩 테스트", LocalDateTime.now(), LocalDateTime.now().plusDays(2)),
-                List.of()
-        );
+        var documentStage = NewStage.builder()
+                .stageType(StageType.DOCUMENT)
+                .submissionStatus(SubmissionStatus.SUBMITTED)
+                .newEtcSchedule(new NewEtcSchedule("코딩 테스트", LocalDateTime.now(), LocalDateTime.now().plusDays(2)))
+                .newInterviewSchedules(List.of())
+                .build();
 
-        var interviewStage = new NewStage(
-                StageType.INTERVIEW,
-                null,
-                new NewEtcSchedule("코딩 테스트", LocalDateTime.now(), LocalDateTime.now().plusDays(2)),
-                List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
-        );
+        var interviewStage = NewStage.builder()
+                .stageType(StageType.INTERVIEW)
+                .newEtcSchedule(new NewEtcSchedule("코딩 테스트", LocalDateTime.now(), LocalDateTime.now().plusDays(2)))
+                .newInterviewSchedules(
+                        List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
+                )
+                .build();
+
+        var finalStage = NewStage.builder()
+                .stageType(StageType.APPLICATION_CLOSE)
+                .finalapplicationStatus(ApplicationStatus.FINAL_PASS)
+                .newEtcSchedule(new NewEtcSchedule("코딩 테스트", LocalDateTime.now(), LocalDateTime.now().plusDays(2)))
+                .newInterviewSchedules(
+                        List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
+                )
+                .build();
 
         // when & then
         applicationPolicyValidator.validateNewStage(etcNewStage);
 
-        // 서류 전형과 면접 전형에 기타 일정이 포함된 경우 예외 발생
+        // 서류 전형, 면접 전형, 지원 종료 에 기타 일정이 포함된 경우 예외 발생
         assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(documentStage))
                 .isInstanceOf(CareerHubException.class)
                 .hasMessage(ErrorStatus.INVALID_ETC_STAGE_RULE.getMessage());
@@ -56,36 +66,48 @@ class ApplicationPolicyValidatorTest extends TestDoubleSupport {
         assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(interviewStage))
                 .isInstanceOf(CareerHubException.class)
                 .hasMessage(ErrorStatus.INVALID_ETC_STAGE_RULE.getMessage());
+
+        assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(finalStage))
+                .isInstanceOf(CareerHubException.class)
+                .hasMessage(ErrorStatus.INVALID_ETC_STAGE_RULE.getMessage());
     }
 
     @Test
     void 면접_일정은_면접_전형일_때만_가능하다() {
         // given
-        var etcNewStage = new NewStage(
-                StageType.ETC,
-                null,
-                null,
-                List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
-        );
+        var etcNewStage = NewStage.builder()
+                .stageType(StageType.ETC)
+                .newInterviewSchedules(
+                        List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
+                )
+                .build();
 
-        var documentStage = new NewStage(
-                StageType.DOCUMENT,
-                SubmissionStatus.SUBMITTED,
-                null,
-                List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
-        );
+        var documentStage = NewStage.builder()
+                .stageType(StageType.DOCUMENT)
+                .submissionStatus(SubmissionStatus.SUBMITTED)
+                .newInterviewSchedules(
+                        List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
+                )
+                .build();
 
-        var interviewStage = new NewStage(
-                StageType.INTERVIEW,
-                null,
-                null,
-                List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
-        );
+        var interviewStage = NewStage.builder()
+                .stageType(StageType.INTERVIEW)
+                .newInterviewSchedules(
+                        List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
+                )
+                .build();
 
+        var finalStage = NewStage.builder()
+                .stageType(StageType.APPLICATION_CLOSE)
+                .finalapplicationStatus(ApplicationStatus.FINAL_PASS)
+                .newInterviewSchedules(
+                        List.of(new NewInterviewSchedule("1차 면접", InterviewType.EXECUTIVE, LocalDateTime.now(), "서울 본사"))
+                )
+                .build();
         // when & then
         applicationPolicyValidator.validateNewStage(interviewStage);
 
-        // 기타 전형과 서류 전형에 면접 일정이 포함된 경우 예외 발생
+        // 기타 전형, 서류 전형, 지원 종료에 면접 일정이 포함된 경우 예외 발생
         assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(etcNewStage))
                 .isInstanceOf(CareerHubException.class)
                 .hasMessage(ErrorStatus.INVALID_SCHEDULE_TYPE_RULE.getMessage());
@@ -93,6 +115,62 @@ class ApplicationPolicyValidatorTest extends TestDoubleSupport {
         assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(documentStage))
                 .isInstanceOf(CareerHubException.class)
                 .hasMessage(ErrorStatus.INVALID_SCHEDULE_TYPE_RULE.getMessage());
+
+        assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(finalStage))
+                .isInstanceOf(CareerHubException.class)
+                .hasMessage(ErrorStatus.INVALID_SCHEDULE_TYPE_RULE.getMessage());
+    }
+
+    @Test
+    void 최종_지원서_상태는_지원_종료_단계일떄만_가능하다() {
+        // given
+        var etcNewStage = NewStage.builder()
+                .stageType(StageType.ETC)
+                .finalapplicationStatus(ApplicationStatus.FINAL_PASS)
+                .newInterviewSchedules(
+                        List.of()
+                )
+                .build();
+
+        var documentStage = NewStage.builder()
+                .stageType(StageType.DOCUMENT)
+                .submissionStatus(SubmissionStatus.SUBMITTED)
+                .finalapplicationStatus(ApplicationStatus.FINAL_PASS)
+                .newInterviewSchedules(
+                        List.of()
+                )
+                .build();
+
+        var interviewStage = NewStage.builder()
+                .stageType(StageType.INTERVIEW)
+                .finalapplicationStatus(ApplicationStatus.FINAL_PASS)
+                .newInterviewSchedules(
+                        List.of()
+                )
+                .build();
+
+        var finalStage = NewStage.builder()
+                .stageType(StageType.APPLICATION_CLOSE)
+                .finalapplicationStatus(ApplicationStatus.FINAL_PASS)
+                .newInterviewSchedules(
+                        List.of()
+                )
+                .build();
+        // when & then
+        applicationPolicyValidator.validateNewStage(finalStage);
+
+        // 기타 전형과 서류 전형에 면접 일정이 포함된 경우 예외 발생
+        assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(etcNewStage))
+                .isInstanceOf(CareerHubException.class)
+                .hasMessage(ErrorStatus.INVALID_FINAL_APPLICATION_STATUS_RULE.getMessage());
+
+        assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(documentStage))
+                .isInstanceOf(CareerHubException.class)
+                .hasMessage(ErrorStatus.INVALID_FINAL_APPLICATION_STATUS_RULE.getMessage());
+
+        assertThatThrownBy(() -> applicationPolicyValidator.validateNewStage(interviewStage))
+                .isInstanceOf(CareerHubException.class)
+                .hasMessage(ErrorStatus.INVALID_FINAL_APPLICATION_STATUS_RULE.getMessage());
     }
 
 }
