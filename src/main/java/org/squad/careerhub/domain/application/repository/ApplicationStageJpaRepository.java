1f003a80821053c2382a001e44036c8cb824d641
Hm-source <swo8852@gmail.com>
2025-12-26 02:45:08 +0900

    Feat[#30]: 지원서, 전형 검색

--- src/main/java/org/squad/careerhub/domain/application/repository/ApplicationStageJpaRepository.java
+++ src/main/java/org/squad/careerhub/domain/application/repository/ApplicationStageJpaRepository.java
@@ -1,10 +1,13 @@
 package org.squad.careerhub.domain.application.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.squad.careerhub.domain.application.entity.ApplicationStage;
+import org.squad.careerhub.domain.application.entity.StageType;
 
 public interface ApplicationStageJpaRepository extends JpaRepository<ApplicationStage, Long> {
 
     List<ApplicationStage> findByApplicationId(Long applicationId);
+    Optional<ApplicationStage> findByApplicationIdAndStageType(Long applicationId, StageType stageType);
 }
