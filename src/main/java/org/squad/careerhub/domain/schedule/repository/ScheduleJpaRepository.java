ab4cf38a81ebd53c7b139c5693c37eaad2608ec2
Hm-source <swo8852@gmail.com>
2025-12-28 02:05:03 +0900

    merge develop

--- src/main/java/org/squad/careerhub/domain/schedule/repository/ScheduleJpaRepository.java
+++ src/main/java/org/squad/careerhub/domain/schedule/repository/ScheduleJpaRepository.java
@@ -1,8 +1,11 @@
 package org.squad.careerhub.domain.schedule.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.squad.careerhub.domain.application.entity.ApplicationStage;
 import org.squad.careerhub.domain.schedule.entity.Schedule;
 
 public interface ScheduleJpaRepository extends JpaRepository<Schedule, Long> {
 
+    List<Schedule> findByApplicationStageInAndAuthorId(List<ApplicationStage> applicationStages, Long authorId);
 }
