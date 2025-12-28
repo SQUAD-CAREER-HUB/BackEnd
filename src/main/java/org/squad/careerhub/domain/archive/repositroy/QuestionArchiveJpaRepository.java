232bdabc977a311ee0d4fcc9211ab1711e00ee04
kwakseobang <iii6602@naver.com>
2025-12-28 16:49:04 +0900

    Refactor[#60]: 불필요한 findByApplicationIdAndStatus 메서드 및 관련 import 제거

--- src/main/java/org/squad/careerhub/domain/archive/repositroy/QuestionArchiveJpaRepository.java
+++ src/main/java/org/squad/careerhub/domain/archive/repositroy/QuestionArchiveJpaRepository.java
@@ -1,21 +1,8 @@
 package org.squad.careerhub.domain.archive.repositroy;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.squad.careerhub.domain.archive.entity.QuestionArchive;
-import org.squad.careerhub.global.entity.EntityStatus;
 
 public interface QuestionArchiveJpaRepository extends JpaRepository<QuestionArchive, Long> {
-
-    @Query("SELECT qa FROM QuestionArchive qa " +
-            "JOIN FETCH qa.interviewQuestion iq JOIN FETCH iq.interviewReview ir " +
-            "WHERE qa.application.id = :applicationId AND qa.status = :status"
-    )
-    List<QuestionArchive> findByApplicationIdAndStatus(
-            @Param("applicationId") Long applicationId,
-            @Param("status") EntityStatus status
-    );
 
 }
