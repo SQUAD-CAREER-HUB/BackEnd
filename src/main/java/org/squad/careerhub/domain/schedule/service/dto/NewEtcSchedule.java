e2cca705dc6ea9a3097778155afc215938b67568
Hm-source <swo8852@gmail.com>
2025-12-30 01:34:11 +0900

    Refactor[#68]: StageType 제거

--- src/main/java/org/squad/careerhub/domain/schedule/service/dto/NewEtcSchedule.java
+++ src/main/java/org/squad/careerhub/domain/schedule/service/dto/NewEtcSchedule.java
@@ -2,12 +2,12 @@ package org.squad.careerhub.domain.schedule.service.dto;
 
 import java.time.LocalDateTime;
 import lombok.Builder;
-import org.squad.careerhub.domain.application.entity.StageType;
 
 @Builder
 public record NewEtcSchedule(
-    StageType stageType,
     String scheduleName,
     LocalDateTime startedAt,
     LocalDateTime endedAt
-) { }
+) {
+
+}
--- src/main/java/org/squad/careerhub/domain/schedule/service/dto/NewInterviewSchedule.java
+++ src/main/java/org/squad/careerhub/domain/schedule/service/dto/NewInterviewSchedule.java
@@ -2,13 +2,12 @@ package org.squad.careerhub.domain.schedule.service.dto;
 
 import java.time.LocalDateTime;
 import lombok.Builder;
-import org.squad.careerhub.domain.application.entity.StageType;
 
 @Builder
 public record NewInterviewSchedule(
-    StageType stageType,
     String scheduleName,
     LocalDateTime startedAt,
     String location
 ) {
+
 }
