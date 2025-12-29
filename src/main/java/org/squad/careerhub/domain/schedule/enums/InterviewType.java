83ee21c95e55619c7e1d8bfc76cdc70503893165
Hm-source <swo8852@gmail.com>
2025-12-30 01:55:43 +0900

    Refactor[#68]: InterviewType 원복

---
+++ src/main/java/org/squad/careerhub/domain/schedule/enums/InterviewType.java
@@ -0,0 +1,19 @@
+package org.squad.careerhub.domain.schedule.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum InterviewType {
+
+    TECH("기술 면접"),
+    FIT("컬처핏/인성 면접"),
+    EXECUTIVE("임원 면접"),
+    DESIGN("시스템 디자인 면접"),
+    TEST("라이브 코딩 테스트 면접"),
+    ETC("기타");
+
+    private final String description;
+
+}
