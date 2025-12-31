ace818128c667db6b63f9b2a5744769b570dbf00
kwakseobang <iii6602@naver.com>
2025-12-31 12:51:03 +0900

    Feat[#71]: 지원서 첨부파일 조회 시 ACTIVE 상태만 반환하도록 수정

--- src/main/java/org/squad/careerhub/domain/application/service/ApplicationAttachmentReader.java
+++ src/main/java/org/squad/careerhub/domain/application/service/ApplicationAttachmentReader.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.squad.careerhub.domain.application.entity.ApplicationAttachment;
 import org.squad.careerhub.domain.application.repository.ApplicationAttachmentJpaRepository;
+import org.squad.careerhub.global.entity.EntityStatus;
 
 @RequiredArgsConstructor
 @Component
@@ -13,7 +14,7 @@ public class ApplicationAttachmentReader {
     private final ApplicationAttachmentJpaRepository applicationAttachmentJpaRepository;
 
     public List<ApplicationAttachment> findAttachments(Long applicationId) {
-        return applicationAttachmentJpaRepository.findAllByApplicationId(applicationId);
+        return applicationAttachmentJpaRepository.findAllByApplicationIdAndStatus(applicationId, EntityStatus.ACTIVE);
     }
 
 }
