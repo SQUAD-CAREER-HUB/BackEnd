8d9e40528429135315405355b57ccd1b3c7a5792
kwakseobang <iii6602@naver.com>
2026-01-04 23:44:06 +0900

    Setting[#79]: 테스트 프로파일을 활성화하여 통합 및 테스트 더블 지원 클래스 구성

--- src/test/java/org/squad/careerhub/TestDoubleSupport.java
+++ src/test/java/org/squad/careerhub/TestDoubleSupport.java
@@ -2,7 +2,9 @@ package org.squad.careerhub;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 public class TestDoubleSupport {
 
