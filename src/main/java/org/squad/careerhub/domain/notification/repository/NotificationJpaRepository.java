62febe51633209276b3d5ba6186fac1f434f06fb
Hm-source <swo8852@gmail.com>
2026-01-11 21:43:36 +0900

    Feat[#63]: 알림 repository

--- src/main/java/org/squad/careerhub/domain/notification/repository/NotificationJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.squad.careerhub.domain.notification.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.squad.careerhub.domain.notification.entity.Notification;
-
-public interface NotificationJpaRepository extends JpaRepository<Notification, Long> {
-
-}
